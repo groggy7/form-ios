97fd8d82aee6d1959f72ef60bfa711cdee38c1b9
groggy7 <serhatkilbas127@gmail.com>
2026-09-11 14:42:58 +0300

    Lock app orientation to portrait only

--- FormApp/App/FormApp.swift
+++ FormApp/App/FormApp.swift
@@ -1,7 +1,19 @@
 import SwiftUI
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        .portrait
+    }
+}
 
 @main
 struct FormApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             RootView()
